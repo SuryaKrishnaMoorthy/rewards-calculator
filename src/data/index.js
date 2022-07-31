46fd9e52ddba89166cea5a135a4d7e584a5a8343
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2022-07-31 17:08:12 -0400

    add data

---
+++ src/data/index.js
@@ -0,0 +1,128 @@
+export const DATA = [
+  {
+    id: 1,
+    customerName: "Nick",
+    transactions: [
+      {
+        date: "01-01-2022",
+        amount: 120,
+      },
+      {
+        date: "01-02-2022",
+        amount: 20,
+      },
+      {
+        date: "01-04-2022",
+        amount: 200,
+      },
+      {
+        date: "02-01-2022",
+        amount: 130,
+      },
+      {
+        date: "02-02-2022",
+        amount: 30,
+      },
+      {
+        date: "02-04-2022",
+        amount: 200,
+      },
+      {
+        date: "03-01-2022",
+        amount:220,
+      },
+      {
+        date: "03-02-2022",
+        amount: 10,
+      },
+      {
+        date: "03-04-2022",
+        amount: 50,
+      },
+    ],
+  },
+	{
+    id: 2,
+    customerName: "Jane",
+    transactions: [
+      {
+        date: "01-01-2022",
+        amount: 130,
+      },
+      {
+        date: "01-02-2022",
+        amount: 120,
+      },
+      {
+        date: "01-04-2022",
+        amount: 200,
+      },
+      {
+        date: "02-01-2022",
+        amount: "130",
+      },
+      {
+        date: "02-02-2022",
+        amount: 30,
+      },
+      {
+        date: "02-04-2022",
+        amount: 200,
+      },
+      {
+        date: "03-01-2022",
+        amount: 230,
+      },
+      {
+        date: "03-02-2022",
+        amount: 10,
+      },
+      {
+        date: "03-04-2022",
+        amount: 50,
+      },
+    ],
+  },
+	{
+    id: 1,
+    customerName: "Nick",
+    transactions: [
+      {
+        date: "01-01-2022",
+        amount: 120,
+      },
+      {
+        date: "01-02-2022",
+        amount: 20,
+      },
+      {
+        date: "01-04-2022",
+        amount: 200,
+      },
+      {
+        date: "02-01-2022",
+        amount: 130,
+      },
+      {
+        date: "02-02-2022",
+        amount: 30,
+      },
+      {
+        date: "02-04-2022",
+        amount: 300,
+      },
+      {
+        date: "03-01-2022",
+        amount: 200,
+      },
+      {
+        date: "03-02-2022",
+        amount: 10,
+      },
+      {
+        date: "03-04-2022",
+        amount:50,
+      },
+    ],
+  },
+];
