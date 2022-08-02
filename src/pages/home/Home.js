c4905a0a7b68b352f1feaa8f0108e4b511711c29
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2022-08-02 07:03:47 -0400

    add home page and css

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Dropdown from "../../components/dropdown/Dropdown";
+import RewardsTable from "../../components/table/RewardsTable";
+import { DATA } from "../../data";
+import { getTableData } from "../../utils";
 import "./home.css";
 
 function Home() {
-  return <main className="home">Home</main>;
+  const [selected, setSelected] = useState("1");
+  const [rewards, setRewards] = useState({});
+
+  useEffect(() => {
+    const selectedCustomer = DATA.filter(
+      (customer) => customer.id === parseInt(selected)
+    );
+    const rewards = getTableData(selectedCustomer[0].transactions);
+    setRewards(rewards);
+  }, [selected]);
+
+  const handleDropdown = (e) => {
+    setSelected(e.target.value);
+  };
+
+  
+  return (
+    <main className="home">
+      <section className="dropdown-section">
+        <Dropdown
+          options={DATA}
+          handleDropdown={handleDropdown}
+          selected={selected}
+        />
+      </section>
+      <section>
+        <RewardsTable 
+          rewards={rewards}
+        />
+      </section>
+    </main>
+  );
 }
 
 export default Home;
